746b00a0b8f8e3d8b5937a27f6b37e4e1d5fc37a
rejosari-space <--unset>
2025-01-26 09:32:04 +0700

    view:login

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/dashboard', function () {
     return view('pages.dashboard', ['type_menu' => 'dashboard']);
+});
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
 });
