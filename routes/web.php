b6a4b445fb962c61f9054cf4dce940a8e7ead580
rejosari-space <--unset>
2025-01-26 12:57:34 +0700

    add login view on provider fortify

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard', ['type_menu' => 'dashboard']);
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
+
+Route::middleware(['auth'])->group(function () {
+   Route::get('/home', function () {
+    return view('pages.dashboard', ['type_menu' => 'home']);
+})->name('home');
 });
